e2df8cd4fe49ed091dc649b88a02f9d69fd9cbdb
mohammed siddique <naumansiddiqui4@gmail.com>
2021-01-28 16:13:09 -0500

    WT: Capture pi images

--- WaterTank.py
+++ WaterTank.py
@@ -1,70 +1,29 @@
-# Code from" https://www.pyimagesearch.com/2017/06/19/image-difference-with-opencv-and-python/
-# https://picamera.readthedocs.io/en/release-1.10/recipes1.html#capturing-to-an-opencv-object
-from skimage.measure import compare_ssim
-import argparse
-import imutils
-import cv2
-import time
+#!/usr/bin/env	python3
+
+# For working with Python Camera: https://medium.com/@petehouston/capture-images-from-raspberry-pi-camera-module-using-picamera-505e9788d609
+
 import picamera
+import os
+import argparse
 
-with picamera.PiCamera() as camera:
-    camera.resolution = (1024, 768)
-    camera.start_preview()
-    # Camera warm-up time
-    time.sleep(2)
-    camera.capture('first.jpg')
+def capture_images():
+    camera = picamera.PiCamera()
+    if not os.path.exists(os.path.join(os.getcwd(), "Images")):
+        os.mkdir(os.path.join(os.getcwd(), "Images"))
+    images = os.listdir(os.path.join(os.getcwd(), "Images"))
+    test_image = "snapshot.jpg"
+    count = 0
+    for image in images:
+        if test_image == image:
+            test_image = "snapshot" + str(0) + ".jpg"
+            count += 1
+    camera.capture(test_image)
 
-print("First image done")
-time.sleep(2)
 
+if __name__ == "__main__":
 
-with picamera.PiCamera() as camera:
-    camera.resolution = (1024, 768)
-    camera.start_preview()
-    # Camera warm-up time
-    time.sleep(2)
-    camera.capture('second.jpg')
-print("Second image done")
-# construct the argument parse and parse the arguments
-#ap = argparse.ArgumentParser()
-#ap.add_argument("-f", "--first", required=True,
-#	help="first input image")
-#ap.add_argument("-s", "--second", required=True,
-#	help="second input image")
-#args = vars(ap.parse_args())
+    parser = argparse.ArgumentParser(description="Raspberry Pi Water Tank Demo", prog="pi")
+    subparsers = parser.add_subparsers()
 
-# load the two input images
-imageA = cv2.imread('first.jpg')
-imageB = cv2.imread('second.jpg')
-# convert the images to grayscale
-grayA = cv2.cvtColor(imageA, cv2.COLOR_BGR2GRAY)
-grayB = cv2.cvtColor(imageB, cv2.COLOR_BGR2GRAY)
-
-# compute the Structural Similarity Index (SSIM) between the two
-# images, ensuring that the difference image is returned
-(score, diff) = compare_ssim(grayA, grayB, full=True)
-diff = (diff * 255).astype("uint8")
-print("SSIM: {}".format(score))
-
-# threshold the difference image, followed by finding contours to
-# obtain the regions of the two input images that differ
-thresh = cv2.threshold(diff, 0, 255,
-	cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
-cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
-	cv2.CHAIN_APPROX_SIMPLE)
-cnts = imutils.grab_contours(cnts)
-
-# loop over the contours
-for c in cnts:
-	# compute the bounding box of the contour and then draw the
-	# bounding box on both input images to represent where the two
-	# images differ
-	(x, y, w, h) = cv2.boundingRect(c)
-	cv2.rectangle(imageA, (x, y), (x + w, y + h), (0, 0, 255), 2)
-	cv2.rectangle(imageB, (x, y), (x + w, y + h), (0, 0, 255), 2)
-# show the output images
-cv2.imshow("Original", imageA)
-cv2.imshow("Modified", imageB)
-cv2.imshow("Diff", diff)
-cv2.imshow("Thresh", thresh)
-cv2.waitKey(0)
+    deleted_parser = subparsers.add_parser("capture", help="Capture Images")
+    deleted_parser.set_defaults(func=capture_images)

f27a7bedd7ceade2da1fd28f172f0ef81081c72d
mohammed siddique <naumansiddiqui4@gmail.com>
2021-02-24 14:39:10 -0500

    test image

--- WaterTank.py
+++ WaterTank.py
@@ -1,8 +1,12 @@
-#!/usr/bin/env	python3
+#!/usr/bin/env	python3.7
 
 # For working with Python Camera: https://medium.com/@petehouston/capture-images-from-raspberry-pi-camera-module-using-picamera-505e9788d609
+# TODO:Test the below codes
+# https://www.pyimagesearch.com/2014/09/15/python-compare-two-images/
+# https://www.pyimagesearch.com/2017/06/19/image-difference-with-opencv-and-python/
+# https://www.thepythoncode.com/article/contour-detection-opencv-python: Contour Detection
 
-import picamera
+# import picamera
 import os
 import argparse
 import sys
@@ -11,83 +15,101 @@ import time
 from PIL import Image, ImageOps
 import cv2
 import numpy as np
-from skimage.measure import structural_similarity as ssim
+from skimage import measure
+import matplotlib.pyplot as plt
 
 debug = True
 
-def capture_images():
+def capture_images(**kw):
     camera = picamera.PiCamera()
     if not os.path.exists(os.path.join(os.getcwd(), "Images")):
         os.mkdir(os.path.join(os.getcwd(), "Images"))
-    images = os.listdir(os.path.join(os.getcwd(), "Images"))
-    if debug: sys.stdout.write("images {}\n".format(images))
-    count = 0
-    for image in images:
-        match = re.findall(r'\d+', image)
-        if debug: sys.stdout.write("digit from image: {}\n".format(match))
-        if match and int(match[0]) >= count:
-            count = int(match[0]) + 1
-
-    test_image = "snapshot" + str(count) + ".jpg"
-
+    if not kw["out"]:
+        images = os.listdir(os.path.join(os.getcwd(), "Images"))
+        if debug: sys.stdout.write("images {}\n".format(images))
+        count = 0
+        for image in images:
+            match = re.findall(r'\d+', image)
+            if debug: sys.stdout.write("digit from image: {}\n".format(match))
+            if match and int(match[0]) >= count:
+                count = int(match[0]) + 1
+        test_image = "snapshot" + str(count) + ".jpg"
+    else:
+        test_image = kw["out"]
     camera.start_preview()
     time.sleep(2)
     if debug: sys.stdout.write("Writing images to: {}\n".format(os.path.join(os.getcwd(), "Images", test_image)))
     camera.capture(os.path.join(os.getcwd(), "Images",test_image))
     camera.stop_preview()
 
-def convert_to_greyscale():
-    images = os.listdir(os.path.join(os.getcwd(), "Images"))
-    for image in images:
-        im1 = Image.open(os.path.join(os.getcwd(), "Images", image))
-        # applying greyscale method
-        im2 = ImageOps.grayscale(im1)
-        im2.show()
-        match = re.findall(r'\d+', image)
-        if match:
-            im2.save(os.path.join(os.getcwd(), "Images", "greyscale" + match[0] + ".jpg"))
+def convert_to_greyscale(**kw):
+    im1 = Image.open(os.path.join(os.getcwd(), "Images", kw["f"]))
+    im2 = ImageOps.grayscale(im1)
+    im2.save(os.path.join(os.getcwd(), "Images", "greyscale" + ".jpg"))
+
+def compare_two_images(**kw):
+    try:
+        original =  cv2.imread(os.path.join(os.getcwd(), "Images", kw["f1"]))
+        duplicate =  cv2.imread(os.path.join(os.getcwd(), "Images", kw["f2"]))
+
+        if original.shape == duplicate.shape:
+            if debug: sys.stdout.write("Color images have same size and channels\n")
+
+        difference = cv2.subtract(original, duplicate)
+
+        b, g, r = cv2.split(difference)
+        if cv2.countNonZero(b) == 0 and cv2.countNonZero(g) == 0 and cv2.countNonZero(r) == 0:
+            if debug: sys.stdout.write("Color images are completely Equal")
         else:
-            im2.save(os.path.join(os.getcwd(), "Images", "greyscale" + ".jpg"))
+            if debug: sys.stdout.write("blue difference: {}\n".format(cv2.countNonZero(b)))
+            if debug: sys.stdout.write("green difference: {}\n".format(cv2.countNonZero(g)))
+            if debug: sys.stdout.write("red difference: {}\n".format(cv2.countNonZero(r)))
+    except Exception as e:
+        sys.stderr.write("compare_two_images: " + str(e))
 
-def compare_two_images():
-    original =  cv2.imread(os.path.join(os.getcwd(), "Images", "snapshot3.jpg"))
-    duplicate =  cv2.imread(os.path.join(os.getcwd(), "Images", "snapshot4.jpg"))
+def find_image_contours(**kw):
+    print(os.path.join(os.getcwd(), "Images", kw["f"]))
+    image = cv2.imread(os.path.join(os.getcwd(), "Images", kw["f"]))
+    # convert to RGB
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    # convert to grayscale
+    gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    # create a binary thresholded image
+    _, binary = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY_INV)
+    # show it
+    plt.imshow(binary, cmap="gray")
+    plt.show()
+    '''
+    # find the contours from the thresholded image
+    contours, hierarchy = cv2.findContours(binary, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    # draw all contours
+    image = cv2.drawContours(image, contours, -1, (0, 255, 0), 2)
+    # show the image with the drawn contours
+    plt.imshow(image)
+    plt.show()
+    '''
 
-    grey_original =  cv2.imread(os.path.join(os.getcwd(), "Images", "greyscale3.jpg"))
-    grey_duplicate =  cv2.imread(os.path.join(os.getcwd(), "Images", "greyscale4.jpg"))
-
-    if original.shape == duplicate.shape:
-        if debug: sys.stdout.write("Color images have same size and channels\n")
-    if grey_original.shape == grey_duplicate.shape:
-        if debug: sys.stdout.write("Greyscale images have same size and channels\n")
-
-    difference = cv2.subtract(original, duplicate)
-    difference_grey = cv2.subtract(grey_original, grey_duplicate)
-
-    b, g, r = cv2.split(difference)
-    if cv2.countNonZero(b) == 0 and cv2.countNonZero(g) == 0 and cv2.countNonZero(r) == 0:
-        if debug: sys.stdout.write("Color images are completely Equal")
-    else:
-        if debug: sys.stdout.write("Color blue difference: {}\n".format(cv2.countNonZero(b)))
-        if debug: sys.stdout.write("Color green difference: {}\n".format(cv2.countNonZero(g)))
-        if debug: sys.stdout.write("Color red difference: {}\n".format(cv2.countNonZero(r)))
-
-    b, g, r = cv2.split(difference_grey)
-    if cv2.countNonZero(b) == 0 and cv2.countNonZero(g) == 0 and cv2.countNonZero(r) == 0:
-        if debug: sys.stdout.write("Greyscale images are completely Equal")
-    else:
-        if debug: sys.stdout.write("Greyscale blue difference: {}\n".format(cv2.countNonZero(b)))
-        if debug: sys.stdout.write("Greyscale green difference: {}\n".format(cv2.countNonZero(g)))
-        if debug: sys.stdout.write("Greyscale red difference: {}\n".format(cv2.countNonZero(r)))
-
-'''
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description="Raspberry Pi Water Tank Demo", prog="pi")
     subparsers = parser.add_subparsers()
 
-    deleted_parser = subparsers.add_parser("capture", help="Capture Images")
-    deleted_parser.set_defaults(func=capture_images)
+    capture_parser = subparsers.add_parser("capture", help="Capture Images")
+    capture_parser.add_argument("out", help="Image Name")
+    capture_parser.set_defaults(func=capture_images)
+
+    grey_parser = subparsers.add_parser("grey", help="Convert images to greyscale")
+    grey_parser.add_argument("--f", metavar="", default="snapshot.jpg", help="Image to covert to greyscale")
+    grey_parser.set_defaults(func=convert_to_greyscale)
+
+    contour_parser = subparsers.add_parser("cont", help="Find Image Contour")
+    contour_parser.add_argument("--f", metavar="", default="snapshot.jpg", help="Image to covert to greyscale")
+    contour_parser.set_defaults(func=find_image_contours)
+
+    compare_parser = subparsers.add_parser("compare", help="Convert two images")
+    compare_parser.add_argument("f1", help="First Image")
+    compare_parser.add_argument("f2", help="Second Image")
+    compare_parser.set_defaults(func=compare_two_images)
 
     args = parser.parse_args()
     try:
@@ -95,8 +117,3 @@ if __name__ == "__main__":
     except Exception as e:
         print(e)
         parser.print_help()
-'''
-
-# capture_images()
-# convert_to_greyscale()
-compare_two_images()
